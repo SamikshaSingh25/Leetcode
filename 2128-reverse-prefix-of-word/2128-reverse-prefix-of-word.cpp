9d97d550382cef9468c863603deaa9d8cc61b5e7
Samiksha Singh <123529290+SamikshaSingh25@users.noreply.github.com>
2024-07-18 17:08:31 +0530

    Time: 3 ms (43.05%), Space: 7.7 MB (9.25%) - LeetHub

---
+++ 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.cpp
@@ -0,0 +1,13 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int index = word.find(ch); // Find the index of the first occurrence of ch
+        if (index != string::npos) { // If ch exists in word
+            reverse(word.begin(), word.begin() + index + 1); // Reverse the segment from 0 to index (inclusive)
+        }
+        return word; // Return the resulting string
+    }
+};
