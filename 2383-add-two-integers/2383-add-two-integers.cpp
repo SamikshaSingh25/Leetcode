fda108971e276bfd2094d4d71e485558f1344f3a
Samiksha Singh <123529290+SamikshaSingh25@users.noreply.github.com>
2024-09-08 13:06:48 +0530

    Time: 0 ms (100%), Space: 7.6 MB (79.27%) - LeetHub

--- 2383-add-two-integers/2383-add-two-integers.cpp
+++ 2383-add-two-integers/2383-add-two-integers.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int sum(int num1, int num2) {
-        int result = num1 + num2;
-        return result;
+        if(num2==0) return num1;
+        return sum(num1^num2,(num1&num2)<<1);
     }
 };
