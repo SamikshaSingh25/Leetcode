19e4f77b44a35dc0328de5ec4434219ef87c8344
Samiksha Singh <123529290+SamikshaSingh25@users.noreply.github.com>
2024-10-04 14:52:31 +0530

    Time: 3 ms (71.31%), Space: 43.3 MB (18.25%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -1,12 +1,36 @@
 class Solution {
     public int longestValidParentheses(String s) {
-        Stack<Integer> st = new Stack<>();
+
+        //my stack approach
+        // Stack<Integer> st = new Stack<>();
+        // //take indexing me starting me 1 se ho (agar -1 hoga toh vo 0th le lega and calculations ko bother bhi ni karega)
+        // st.push(-1);
+        // int max = 0;
+        // for(int i=0;i<s.length();i++){
+        //     char c = s.charAt(i);
+        //     if(c=='('){
+        //         st.push(i);
+        //     }
+        //     else{
+        //         st.pop();
+        //         if(st.isEmpty()){
+        //             st.push(i);
+        //         }
+        //         else{
+        //             int l = i-st.peek();
+        //             max=Math.max(max,l);
+        //         }
+        //     }
+        // }
+        // return max;
+
+        //randomn chinese student approch i saw of deque
+        Deque<Integer> st=new ArrayDeque<>();
         st.push(-1);
-        int max = 0;
+        int ans=0;
         for(int i=0;i<s.length();i++){
-            char c = s.charAt(i);
-            if(c=='('){
-                st.push(i);
+            if(s.charAt(i) == '('){
+                st.push(i); //index is getting pushed
             }
             else{
                 st.pop();
@@ -14,11 +38,11 @@ class Solution {
                     st.push(i);
                 }
                 else{
-                    int l = i-st.peek();
-                    max=Math.max(max,l);
+                    ans = Math.max(ans, i-st.peek());
                 }
             }
         }
-        return max;
+        return ans;
+
     }
 }
