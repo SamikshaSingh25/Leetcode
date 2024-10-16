5c70e8244e9ea752075ee9f4144c7736ff34604c
Samiksha Singh <123529290+SamikshaSingh25@users.noreply.github.com>
2024-10-16 18:21:49 +0530

    Time: 115 ms (94.59%), Space: 62.7 MB (81.42%) - LeetHub

---
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.java
@@ -0,0 +1,31 @@
+class Solution {
+    public long maxKelements(int[] nums, int k) {
+        //brute force not many cases passed
+        // int sum=0;
+        // int n=nums.length-1;
+        // while(k>0){
+        //     Arrays.sort(nums);
+        //     int i = nums[n];
+        //     sum+=i;
+        //     int first = (i+2)/3;
+        //     nums[n]=first;
+        //     k--;
+        // }
+        // return sum;
+
+        ///priority queue solution
+        //making maxheap instead of default min heap in java
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
+        for(int i:nums){
+            pq.offer(i);
+        }
+        long sum=0;
+        for(int i=0;i<k;i++){
+            int n = pq.poll();
+            sum+=n;
+            pq.offer((n+2)/3);
+        }
+        return sum;
+    
+    }
+}
