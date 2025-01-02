fc6b535c6d38360e54eb554afd6ef69dcb493e11
Samiksha Singh <123529290+SamikshaSingh25@users.noreply.github.com>
2025-01-03 00:24:13 +0530

    Time: 3 ms (100%), Space: 85.6 MB (87.24%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
@@ -30,31 +30,61 @@ class Solution {
 
 
     //optimized (prefix sum method)
-    public static boolean isVowel(String s){
+    // public static boolean isVowel(String s){
+    //     if(s==null || s.length()==0){
+    //         return false;
+    //     }
+    //     char firstChar = s.charAt(0);
+    //     char lastChar = s.charAt(s.length()-1);
+    //     return isVowelChar(firstChar) && isVowelChar(lastChar);
+    // }
+    // public static boolean isVowelChar(char c){
+    //     return c == 'a' || c == 'e' || c=='i' || c=='o' || c=='u';
+    // }
+    // public int[] vowelStrings(String[] words, int[][] queries) {
+    //     int ans[] = new int[queries.length];
+    //     int vowelArr[] = new int[words.length + 1];
+    //     for(int i =0;i<words.length ; i++ ){
+    //         if(isVowel(words[i])){
+    //             vowelArr[i+1] = vowelArr[i]+1;
+    //         }
+    //         else{
+    //             vowelArr[i+1] = vowelArr[i];
+    //         }
+    //     }
+    //     for(int i=0;i<queries.length;i++){
+    //         ans[i] = vowelArr[queries[i][1]+1] - vowelArr[queries[i][0]];
+    //     }
+    //     return ans;
+    // }
+    private static boolean isVowel(String s){
         if(s==null || s.length()==0){
             return false;
         }
         char firstChar = s.charAt(0);
         char lastChar = s.charAt(s.length()-1);
-        return isVowelChar(firstChar) && isVowelChar(lastChar);
-    }
-    public static boolean isVowelChar(char c){
-        return c == 'a' || c == 'e' || c=='i' || c=='o' || c=='u';
+
+        if((firstChar == 'a' || firstChar == 'e' || firstChar == 'i' || firstChar == 'o' || firstChar == 'u') && (lastChar == 'a' || lastChar == 'e' || lastChar == 'i' || lastChar == 'o' || lastChar == 'u')){
+            return true;
+        }
+        return false;
     }
     public int[] vowelStrings(String[] words, int[][] queries) {
-        int ans[] = new int[queries.length];
-        int vowelArr[] = new int[words.length + 1];
-        for(int i =0;i<words.length ; i++ ){
+        int[] ans = new int[queries.length];
+        int[] vowel = new int[words.length+1];
+
+        for(int i=0;i<words.length;i++){
             if(isVowel(words[i])){
-                vowelArr[i+1] = vowelArr[i]+1;
+                vowel[i+1] = vowel[i]+1;
             }
             else{
-                vowelArr[i+1] = vowelArr[i];
+                vowel[i+1] = vowel[i];
             }
         }
         for(int i=0;i<queries.length;i++){
-            ans[i] = vowelArr[queries[i][1]+1] - vowelArr[queries[i][0]];
+            ans[i] =vowel[queries[i][1]+1]-vowel[queries[i][0]];
         }
         return ans;
     }
+
 }
