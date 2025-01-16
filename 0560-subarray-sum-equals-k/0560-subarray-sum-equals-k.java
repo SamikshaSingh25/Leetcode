ac14b5f78ce85222fbe5dbdfec3a80ff571c35b9
Samiksha Singh <123529290+SamikshaSingh25@users.noreply.github.com>
2025-01-16 19:10:56 +0530

    Time: 13 ms (99.95%), Space: 47.5 MB (5.48%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,23 +1,34 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        //Brute TL:(O^3) most probably
-        int n = nums.length;
-        int sum=0;
-        int c=0;
-        for(int i=0;i<n;i++){
-            sum=0;
-            for(int j=i;j<n;j++){
-               sum+=nums[j];
-               if(sum==k){
-                c++;
-               }
-            }
-        }
-        // for(int i=0;i<n;i++)
-        //     if(nums[i]==k){
+        //Brute TL:(O(n^2)) most probably
+        // int n = nums.length;
+        // int sum=0;
+        // int c=0;
+        // for(int i=0;i<n;i++){
+        //     sum=0;
+        //     for(int j=i;j<n;j++){
+        //        sum+=nums[j];
+        //        if(sum==k){
         //         c++;
+        //        }
         //     }
         // }
+        // return c;
+
+
+        //Optimised
+        int n = nums.length;
+        int[] ps = new int[n+1];
+        for(int i=0;i<n;i++){
+            ps[i+1] = nums[i] + ps[i]; 
+        }
+        int c=0;
+        HashMap<Integer, Integer> hm = new HashMap<>(n+1);
+
+        for(int p:ps){
+            c+=hm.getOrDefault(p-k,0);
+            hm.merge(p,1,Integer::sum);
+        }
         return c;
     }
 }
