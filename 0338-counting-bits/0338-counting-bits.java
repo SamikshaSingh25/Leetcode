c911bbd47278059d791d831baf8231607cf191c3
Samiksha Singh <123529290+SamikshaSingh25@users.noreply.github.com>
2025-04-21 18:57:06 +0530

    Time: 2 ms (94.93%), Space: 46.7 MB (68%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,9 +1,22 @@
 class Solution {
     public int[] countBits(int n) {
         //with builtin function -> the question explicitly says not to use it
+        // int res[] = new int[n+1];
+        // for(int i=0;i<=n;i++){
+        //     res[i]=Integer.bitCount(i);
+        // }
+        // return res;
+
+        //Approach 2
         int res[] = new int[n+1];
-        for(int i=0;i<=n;i++){
-            res[i]=Integer.bitCount(i);
+        res[0]=0;
+        for(int i=1;i<=n;i++){
+            if(i%2!=0){
+                res[i]=res[i/2]+1;
+            }
+            else{
+                res[i]=res[i/2];
+            }
         }
         return res;
     }
