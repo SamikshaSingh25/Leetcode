259529bdadc16c87b93e6ce340456632e31b2ce7
Samiksha Singh <123529290+SamikshaSingh25@users.noreply.github.com>
2025-04-23 18:37:18 +0530

    Time: 0 ms (100%), Space: 40.4 MB (64.76%) - LeetHub

--- 1054-complement-of-base-10-integer/1054-complement-of-base-10-integer.java
+++ 1054-complement-of-base-10-integer/1054-complement-of-base-10-integer.java
@@ -1,6 +1,13 @@
 class Solution {
     public int bitwiseComplement(int n) {
-        if(n==0) return 1;
-       return ((Integer.highestOneBit(n)<<1)-1)^n; 
+        //1st Approach
+    //     if(n==0) return 1;
+    //    return ((Integer.highestOneBit(n)<<1)-1)^n; 
+
+       //2nd Approach
+       int l = Integer.toBinaryString(n).length();
+       int v=1<<l;
+       int mask = v-1;
+       return mask^n;
     }
 }
